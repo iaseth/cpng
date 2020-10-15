00660db1266c46d8744712cbab3e472ae1ec9f16
Ankur Seth <iankurseth@gmail.com>
2020-10-15 22:55:59 +0530

    Lets avoid circular dep

--- include/cpng_pixel.h
+++ include/cpng_pixel.h
@@ -7,9 +7,6 @@ struct CpngPixel {
 	uint8_t red, green, blue, transparency;
 };
 
-void cpng_pixel_set_color (struct CpngPixel *pixel, struct CpngColor *color);
-void cpng_pixel_get_color (struct CpngPixel *pixel, struct CpngColor *color);
-
 
 
 #endif
--- src/cpng_pixel.c
+++ src/cpng_pixel.c
@@ -2,18 +2,4 @@
 
 
 
-void cpng_pixel_set_color (struct CpngPixel *pixel, struct CpngColor *color) {
-	pixel->red = color->red;
-	pixel->green = color->green;
-	pixel->blue = color->blue;
-	pixel->transparency = color->transparency;
-}
-
-void cpng_pixel_get_color (struct CpngPixel *pixel, struct CpngColor *color) {
-	color->red = pixel->red;
-	color->green = pixel->green;
-	color->blue = pixel->blue;
-	color->transparency = pixel->transparency;
-}
-
 
