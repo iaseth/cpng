9bb6fa26cff294cc5ca3559738dedc91b270cb72
Ankur Seth <iankurseth@gmail.com>
2020-10-15 22:58:55 +0530

    CpngColor methods now update the operand

--- include/cpng_color.h
+++ include/cpng_color.h
@@ -8,24 +8,25 @@ struct CpngColor {
 	char name[20];
 };
 
-struct CpngColor cpng_color_print (struct CpngColor color);
+void cpng_color_print (struct CpngColor *color);
+void cpng_color_copy (struct CpngColor *color, struct CpngColor *copy);
 
-struct CpngColor cpng_color_set_RGB (struct CpngColor color, int red, int green, int blue);
-struct CpngColor cpng_color_set_transparency (struct CpngColor color, int transparency);
+void cpng_color_set_RGB (struct CpngColor *color, int red, int green, int blue);
+void cpng_color_set_transparency (struct CpngColor *color, int transparency);
 
-struct CpngColor cpng_color_set_red (struct CpngColor color, int value);
-struct CpngColor cpng_color_set_green (struct CpngColor color, int value);
-struct CpngColor cpng_color_set_blue (struct CpngColor color, int value);
+void cpng_color_set_red (struct CpngColor *color, int value);
+void cpng_color_set_green (struct CpngColor *color, int value);
+void cpng_color_set_blue (struct CpngColor *color, int value);
 
-struct CpngColor cpng_color_darken (struct CpngColor color);
-struct CpngColor cpng_color_lighten (struct CpngColor color);
+void cpng_color_darken (struct CpngColor *color);
+void cpng_color_lighten (struct CpngColor *color);
 
-struct CpngColor cpng_color_make_opaque (struct CpngColor color);
-struct CpngColor cpng_color_make_transparent (struct CpngColor color);
+void cpng_color_make_opaque (struct CpngColor *color);
+void cpng_color_make_transparent (struct CpngColor *color);
 
-struct CpngColor cpng_color_add_red (struct CpngColor color, int change);
-struct CpngColor cpng_color_add_green (struct CpngColor color, int change);
-struct CpngColor cpng_color_add_blue (struct CpngColor color, int change);
+void cpng_color_add_red (struct CpngColor *color, int change);
+void cpng_color_add_green (struct CpngColor *color, int change);
+void cpng_color_add_blue (struct CpngColor *color, int change);
 
 
 
