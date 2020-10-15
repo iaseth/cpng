61ca65580f01844e9b3e494202bfc265c13a4d89
Ankur Seth <iankurseth@gmail.com>
2020-10-15 18:33:53 +0530

    Added png_image_stuff()

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <math.h>
+#include <malloc.h>
+#include <png.h>
+
+#include "cpng_core.h"
+
+void png_image_stuff () {
+	struct CpngImage *image = get_new_cpng_image(3840, 2160);
+
+	cpng_image_set_author(image, "Igor");
+	cpng_image_set_filename(image, "x.png");
+	cpng_image_set_title(image, "Example");
+
+	cpng_image_print(image);
+	cpng_image_save_to_disk(image);
+	image = delete_cpng_image(image);
+}
 
 int main (int argc, char const *argv[]) {
-	printf("Hello, World!\n");
-	return 0;
+	png_image_stuff();
 }
