42da4efaf6b4f3fb0fb326ba878711a59d19d534
Ankur Seth <iankurseth@gmail.com>
2020-10-15 22:32:49 +0530

    Added struct CpngColor

--- include/cpng_color.h
+++ include/cpng_color.h
@@ -1,6 +1,22 @@
 #ifndef CPNG_COLOR_H
 #define CPNG_COLOR_H
 
+#include <stdint.h>
+
+struct CpngColor {
+	uint8_t red, green, blue, transparency;
+};
+
+struct CpngColor cpng_color_darken (struct CpngColor color);
+struct CpngColor cpng_color_lighten (struct CpngColor color);
+
+struct CpngColor cpng_color_make_opaque (struct CpngColor color);
+struct CpngColor cpng_color_make_transparent (struct CpngColor color);
+
+struct CpngColor cpng_color_add_red (struct CpngColor color, int change);
+struct CpngColor cpng_color_add_green (struct CpngColor color, int change);
+struct CpngColor cpng_color_add_blue (struct CpngColor color, int change);
+
 
 
 #endif
