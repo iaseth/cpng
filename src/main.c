49b7eb9418898a93e16526ffb8bd69af00a34a09
Ankur Seth <iankurseth@gmail.com>
2020-10-16 17:11:34 +0530

    Now using facebook blue

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@ void png_image_stuff () {
 	cpng_image_set_filename(image, "pngs/x.png");
 	cpng_image_set_title(image, "Example");
 
-	struct CpngPixel red = {240, 25, 25};
-	cpng_image_set_foreground_color(image, red);
+	struct CpngPixel facebook = {59, 89, 152};
+	cpng_image_set_foreground_color(image, facebook);
 	//cpng_image_print_details(image);
 
 	cpng_image_print_cursors(image);
