f5a722e025430e49b64d4793694cddf7792b9571
Ankur Seth <iankurseth@gmail.com>
2020-10-16 10:23:23 +0530

    Updated rod and bar methods to use new rectangle

--- lib/cpng_image.c
+++ lib/cpng_image.c
@@ -269,19 +269,23 @@ void cpng_image_add_circle (struct CpngImage *image, int radius) {
 
 
 void cpng_image_add_bar_at_top (struct CpngImage *image, int offset, int thickness) {
-	//cpng_image_add_rectangle(image, offset, 0, image->width, thickness);
+	cpng_image_move_cursor_to_xy(image, (offset + thickness/2), (image->width / 2));
+	cpng_image_add_rectangle(image, image->width, thickness);
 }
 
 void cpng_image_add_bar_at_bottom (struct CpngImage *image, int offset, int thickness) {
-	//cpng_image_add_rectangle(image, (image->height - offset - thickness), 0, image->width, thickness);
+	cpng_image_move_cursor_to_xy(image, (image->height - (offset + thickness/2)), (image->width / 2));
+	cpng_image_add_rectangle(image, image->width, thickness);
 }
 
 void cpng_image_add_rod_at_left (struct CpngImage *image, int offset, int thickness) {
-	//cpng_image_add_rectangle(image, 0, offset, thickness, image->height);
+	cpng_image_move_cursor_to_xy(image, (image->height / 2), (offset + thickness / 2));
+	cpng_image_add_rectangle(image, thickness, image->height);
 }
 
 void cpng_image_add_rod_at_right (struct CpngImage *image, int offset, int thickness) {
-	//cpng_image_add_rectangle(image, 0, (image->width - offset - thickness), thickness, image->height);
+	cpng_image_move_cursor_to_xy(image, (image->height / 2), (image->width - (offset + thickness / 2)));
+	cpng_image_add_rectangle(image, thickness, image->height);
 }
 
 void cpng_image_add_border (struct CpngImage *image, int thickness) {
