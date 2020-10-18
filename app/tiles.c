46cfe50ff0c7ea48130cc3a13cc516d34bc6ad15
Ankur Seth <iankurseth@gmail.com>
2020-10-18 20:46:31 +0530

    Printing small rosewood squares

--- app/tiles.c
+++ app/tiles.c
@@ -14,7 +14,15 @@ void tiles_stuff () {
 	cpng_image_set_filename(image, "pngs/tiles.png");
 	cpng_image_set_title(image, "Tiles");
 
-	//
+	cpng_image_add_color(image, "rosewood");
+	for (int row = 0; row < 18; ++row) {
+		for (int col = 0; col < 32; ++col) {
+			int x = 60 + (row * 120);
+			int y = 60 + (col * 120);
+			cpng_cursor_move_to_xy(image, x, y);
+			cpng_image_add_square(image, 100);
+		}
+	}
 
 	cpng_image_save_to_disk(image);
 
